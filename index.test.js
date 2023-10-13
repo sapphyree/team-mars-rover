5673eba0b2cac44675e4b829031dfd3b5d3191c1
Dzhani Daud <dzhani.daud@caplin.com>
2023-10-13 13:04:37 +0100

    Added Q functionality

--- index.test.js
+++ index.test.js
@@ -1,6 +1,16 @@
 describe("initialTestBlock", () => {
 
-	it("initialTest", () => {
-		expect(true).toBe(true);
+	it("Quering State", () => {
+
+		rover = {
+			x: 1,
+			y:1,
+			direction: 'North',
+			query: function (command) {
+				return "Coordinates: " + this.x + ',' + this.y + ', Direction: ' + this.direction;
+			}
+		}
+
+		expect(rover.query('Q')).toBe("Coordinates: 1,1, Direction: North");
 	})
 })
